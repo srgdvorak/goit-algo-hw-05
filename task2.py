2a1def2a6d33dfb8643fbc4f4daa711fbccc6523
srgdvorak <srgdvorakgmail.com>
2025-08-15 15:20:33 +0200

    22

--- task2.py
+++ task2.py
@@ -2,16 +2,9 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    """
-    Генерує всі дійсні числа з тексту, які відокремлені лише пробілами з обох боків.
-    Числа на початку чи в кінці тексту ігноруються.
-    """
-    pattern = r"(?<= )\d+\.\d+(?= )|(?<= )\d+(?= )"
+    pattern = r" \d+\.\d+ | \d+ "
     for match in re.finditer(pattern, text):
         yield float(match.group())
 
 def sum_profit(text: str, func: Callable):
-    """
-    Підсумовує всі числа, знайдені за допомогою генератора func.
-    """
     return sum(func(text))
--- task4.py
+++ task4.py
@@ -19,6 +19,15 @@ def add_contact(data, contacts):
 
 
 @input_error
+def change_contact(data, contacts):
+    name, phone = data.split()
+    if name not in contacts:
+        raise KeyError
+    contacts[name] = phone
+    return "Contact updated."
+
+
+@input_error
 def get_phone(data, contacts):
     return f"{data}: {contacts[data]}"
 
@@ -29,13 +38,17 @@ def show_all(contacts):
     return '\n'.join(f"{n}: {p}" for n, p in contacts.items())
 
 
+def hello():
+    return "How can I help you?"
+
+
 def main():
     contacts = {}
     print("Welcome to the assistant bot!")
-    
+
     while True:
         user_input = input("Enter a command: ").strip()
-        if user_input.lower() in ("exit", "close"):
+        if user_input.lower() in ("exit", "close", "good bye"):
             print("Good bye!")
             break
 
@@ -45,9 +58,17 @@ def main():
 
         if command == "add":
             print(add_contact(data, contacts))
+        elif command == "change":
+            print(change_contact(data, contacts))
         elif command == "phone":
             print(get_phone(data, contacts))
         elif command == "all":
             print(show_all(contacts))
+        elif command == "hello":
+            print(hello())
         else:
             print("Unknown command.")
+
+
+if __name__ == "__main__":
+    main()
